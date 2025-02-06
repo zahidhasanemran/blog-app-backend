1a02fd661b3934b6acbd1d0cd4e9a03d78a47379
zahidhasanemran <zahid.emran@thealtcom.com>
2025-02-06 21:35:46 +0600

    index to server in package . json file

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-
 dotenv.config();
 
 const app = express();
 
+dotenv.config();
 // Middleware
 app.use(cors());
 app.use(helmet());
@@ -16,6 +16,9 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api', (req, res) => {
+  res.write("Hello Bangladesh")
+  });
 
 // Connect to MongoDB
 connectDB();
--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log('Mongo URI:', process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
